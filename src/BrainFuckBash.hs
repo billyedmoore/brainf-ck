067d34e8842e7a1df39a6e300132e8d7f524f927
Billy Moore <billyedmoore@yahoo.com>
2025-12-31 16:25:34 +0000

    BrainFuck to bash transpiler.

--- src/BrainFuckBash.hs
+++ src/BrainFuckBash.hs
@@ -1,4 +1,21 @@
 module BrainFuckBash (compile) where
 
 compile :: String -> String
-compile _ = "#!/bin/bash\necho Hello World;"
+compile s = prelude ++ concatMap handleChar (removeNonBFChars s)
+
+removeNonBFChars :: String -> String
+removeNonBFChars = filter (`elem` "<>+-[],.")
+
+prelude :: String
+prelude = "#!/bin/bash\nbytes=(0);\ni=0;\n"
+
+handleChar :: Char -> String
+handleChar '>' = "((i++));\n"
+handleChar '<' = "((i--));\n"
+handleChar '+' = "((bytes[i] = (bytes[i] + 1) % 256));\n"
+handleChar '-' = "((bytes[i] = (bytes[i] - 1 + 256) % 256));\n"
+handleChar '.' = "printf \"\\\\$(printf '%03o' \"${bytes[i]}\")\";\n"
+handleChar ',' = "IFS= read -rn 1 -s char && printf '%d' \"'${char:-$\\n}\""
+handleChar '[' = "while ((bytes[i] != 0))\ndo\n"
+handleChar ']' = "done\n"
+handleChar _ = error "Unexpected Char"
