5136f702cd8f4a88a8e512129c88a4766f30504f
Billy Moore <billyedmoore@yahoo.com>
2026-01-01 19:09:50 +0000

    Overhall Bash transpiler implementation to use the AST.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,22 @@
 module Main (main) where
 
 import BrainFuck.Bash qualified as Bash
+import BrainFuck.Parse (parse)
 import System.Environment (getArgs)
+import System.Exit (die)
 
 main :: IO ()
 main = do
   args <- getArgs
-  result <- case args of
-    [file] -> fmap Bash.compile (readFile file)
-    [] -> error "Provide a .bf file to compile."
-    _ -> error "Usage Error."
-  putStrLn result
+
+  case args of
+    [filePath] -> do
+      content <- readFile filePath
+
+      case parse content of
+        Left err ->
+          die $ "Parse Error: " ++ show err
+        Right ast -> do
+          let result = Bash.compile ast
+          putStrLn result
+    _ -> putStrLn "Usage: brainf-ck <bf-file>"
