761ea6d050faa48942416e071a2aa7eafbd9511b
Billy Moore <billyedmoore@yahoo.com>
2026-01-01 17:01:17 +0000

    Parse bf into an AST.

--- src/BrainFuck/Parse.hs
+++ src/BrainFuck/Parse.hs
@@ -1,4 +1,4 @@
-module BrainFuck.Parse (AST (..)) where
+module BrainFuck.Parse (AST (..), parse) where
 
 data AST
   = DataIncrement Int
@@ -8,3 +8,39 @@ data AST
   | GetChar
   | PutChar
   | Loop [AST]
+  deriving (Eq, Show)
+
+parse :: String -> [AST]
+parse = squash . basicParse . removeNonBFChars
+
+removeNonBFChars :: String -> String
+removeNonBFChars = filter (`elem` "<>+-[],.")
+
+basicParse :: String -> [AST]
+basicParse ('+' : xs) = DataIncrement 1 : basicParse xs
+basicParse ('-' : xs) = DataDecrement 1 : basicParse xs
+basicParse ('>' : xs) = PtrIncrement 1 : basicParse xs
+basicParse ('<' : xs) = PtrDecrement 1 : basicParse xs
+basicParse ('.' : xs) = PutChar : basicParse xs
+basicParse (',' : xs) = GetChar : basicParse xs
+basicParse ('[' : xs) =
+  let (loopBody, rest) = findLoopEnd xs "" 0
+   in Loop (basicParse loopBody) : basicParse rest
+  where
+    findLoopEnd :: String -> String -> Int -> (String, String)
+    findLoopEnd (']' : rest) loopBody 0 = (reverse loopBody, rest)
+    findLoopEnd (']' : rest) loopBody n = findLoopEnd rest (']' : loopBody) (n - 1)
+    findLoopEnd ('[' : rest) loopBody n = findLoopEnd rest ('[' : loopBody) (n + 1)
+    findLoopEnd (c : rest) loopBody n = findLoopEnd rest (c : loopBody) n
+    findLoopEnd [] _ _ = error "Loop does not end."
+basicParse (']' : _) = error "Loop ends before it begins."
+basicParse [] = []
+basicParse (c : _) = error ("Unexpected symbol - " ++ show c ++ ".")
+
+squash :: [AST] -> [AST]
+squash (DataIncrement n1 : DataIncrement n2 : xs) = squash (DataIncrement (n1 + n2) : xs)
+squash (DataDecrement n1 : DataDecrement n2 : xs) = squash (DataDecrement (n1 + n2) : xs)
+squash (PtrIncrement n1 : PtrIncrement n2 : xs) = squash (PtrIncrement (n1 + n2) : xs)
+squash (PtrDecrement n1 : PtrDecrement n2 : xs) = squash (PtrDecrement (n1 + n2) : xs)
+squash (a : xs) = a : squash xs
+squash [] = []
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,80 @@
-module Main (main) where
+module Main where
+
+import BrainFuck.Parse (AST (..), parse)
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+-- These tests are largely AI written
+
+tests :: TestTree
+tests =
+  testGroup
+    "Brainfuck.Parse Unit Tests"
+    [ basicInstructions,
+      collapsingLogic,
+      loopTests,
+      sanitizationTests
+    ]
+
+basicInstructions :: TestTree
+basicInstructions =
+  testGroup
+    "Basic Instructions"
+    [ testCase "Parse single DataIncrement" $
+        parse "+" @?= [DataIncrement 1],
+      testCase "Parse single DataDecrement" $
+        parse "-" @?= [DataDecrement 1],
+      testCase "Parse single PtrIncrement" $
+        parse ">" @?= [PtrIncrement 1],
+      testCase "Parse single PtrDecrement" $
+        parse "<" @?= [PtrDecrement 1],
+      testCase "Parse IO tokens" $
+        parse ".," @?= [PutChar, GetChar]
+    ]
+
+collapsingLogic :: TestTree
+collapsingLogic =
+  testGroup
+    "Collapsing Logic"
+    [ testCase "Collapse multiple pluses" $
+        parse "+++" @?= [DataIncrement 3],
+      testCase "Collapse multiple minuses" $
+        parse "----" @?= [DataDecrement 4],
+      testCase "Collapse pointer moves" $
+        parse ">>>" @?= [PtrIncrement 3],
+      testCase "Do not collapse different tokens" $
+        parse "+>" @?= [DataIncrement 1, PtrIncrement 1]
+    ]
+
+loopTests :: TestTree
+loopTests =
+  testGroup
+    "Loop and Nesting"
+    [ testCase "Empty loop" $
+        parse "[]" @?= [Loop []],
+      testCase "Simple loop body" $
+        parse "[+]" @?= [Loop [DataIncrement 1]],
+      testCase "Nested loops" $
+        parse "[>[-] <]"
+          @?= [ Loop
+                  [ PtrIncrement 1,
+                    Loop [DataDecrement 1],
+                    PtrDecrement 1
+                  ]
+              ],
+      testCase "Multiple loops in sequence" $
+        parse "[+][-]" @?= [Loop [DataIncrement 1], Loop [DataDecrement 1]]
+    ]
+
+sanitizationTests :: TestTree
+sanitizationTests =
+  testGroup
+    "Sanitization (Comments/Whitespace)"
+    [ testCase "Ignore letters and spaces" $
+        parse "+ + hello >" @?= [DataIncrement 2, PtrIncrement 1],
+      testCase "Ignore newlines" $
+        parse "+\n+" @?= [DataIncrement 2]
+    ]
