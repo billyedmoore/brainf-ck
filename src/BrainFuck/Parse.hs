b3e309d7f0d35b599c279b7c2748956b7d9ccb8a
Billy Moore <billyedmoore@yahoo.com>
2026-01-01 18:06:38 +0000

    Add proper errors to parsing.

--- src/BrainFuck/Parse.hs
+++ src/BrainFuck/Parse.hs
@@ -1,46 +1,54 @@
-module BrainFuck.Parse (AST (..), parse) where
+module BrainFuck.Parse (BrainFuckAST (..), ParseError (..), parse) where
 
-data AST
+data ParseError
+  = UnexpectedSymbol Char
+  | UnmatchedLoopOpen
+  | UnmatchedLoopClose
+  deriving (Eq, Show)
+
+data BrainFuckAST
   = DataIncrement Int
   | DataDecrement Int
   | PtrIncrement Int
   | PtrDecrement Int
   | GetChar
   | PutChar
-  | Loop [AST]
+  | Loop [BrainFuckAST]
   deriving (Eq, Show)
 
-parse :: String -> [AST]
-parse = squash . basicParse . removeNonBFChars
+parse :: String -> Either ParseError [BrainFuckAST]
+parse = fmap squash . basicParse . removeNonBFChars
 
 removeNonBFChars :: String -> String
 removeNonBFChars = filter (`elem` "<>+-[],.")
 
-basicParse :: String -> [AST]
-basicParse ('+' : xs) = DataIncrement 1 : basicParse xs
-basicParse ('-' : xs) = DataDecrement 1 : basicParse xs
-basicParse ('>' : xs) = PtrIncrement 1 : basicParse xs
-basicParse ('<' : xs) = PtrDecrement 1 : basicParse xs
-basicParse ('.' : xs) = PutChar : basicParse xs
-basicParse (',' : xs) = GetChar : basicParse xs
-basicParse ('[' : xs) =
-  let (loopBody, rest) = findLoopEnd xs "" 0
-   in Loop (basicParse loopBody) : basicParse rest
+basicParse :: String -> Either ParseError [BrainFuckAST]
+basicParse ('+' : xs) = (DataIncrement 1 :) <$> basicParse xs
+basicParse ('-' : xs) = (DataDecrement 1 :) <$> basicParse xs
+basicParse ('>' : xs) = (PtrIncrement 1 :) <$> basicParse xs
+basicParse ('<' : xs) = (PtrDecrement 1 :) <$> basicParse xs
+basicParse ('.' : xs) = (PutChar :) <$> basicParse xs
+basicParse (',' : xs) = (GetChar :) <$> basicParse xs
+basicParse ('[' : xs) = do
+  (loopBody, rest) <- findLoopEnd xs "" 0
+  parsedBody <- basicParse loopBody
+  parsedRest <- basicParse rest
+  return (Loop parsedBody : parsedRest)
   where
-    findLoopEnd :: String -> String -> Int -> (String, String)
-    findLoopEnd (']' : rest) loopBody 0 = (reverse loopBody, rest)
+    findLoopEnd :: String -> String -> Int -> Either ParseError (String, String)
+    findLoopEnd (']' : rest) loopBody 0 = Right (reverse loopBody, rest)
     findLoopEnd (']' : rest) loopBody n = findLoopEnd rest (']' : loopBody) (n - 1)
     findLoopEnd ('[' : rest) loopBody n = findLoopEnd rest ('[' : loopBody) (n + 1)
     findLoopEnd (c : rest) loopBody n = findLoopEnd rest (c : loopBody) n
-    findLoopEnd [] _ _ = error "Loop does not end."
-basicParse (']' : _) = error "Loop ends before it begins."
-basicParse [] = []
-basicParse (c : _) = error ("Unexpected symbol - " ++ show c ++ ".")
+    findLoopEnd [] _ _ = Left UnmatchedLoopOpen
+basicParse (']' : _) = Left UnmatchedLoopClose
+basicParse [] = Right []
+basicParse (c : _) = Left $ UnexpectedSymbol c
 
-squash :: [AST] -> [AST]
+squash :: [BrainFuckAST] -> [BrainFuckAST]
 squash (DataIncrement n1 : DataIncrement n2 : xs) = squash (DataIncrement (n1 + n2) : xs)
 squash (DataDecrement n1 : DataDecrement n2 : xs) = squash (DataDecrement (n1 + n2) : xs)
 squash (PtrIncrement n1 : PtrIncrement n2 : xs) = squash (PtrIncrement (n1 + n2) : xs)
 squash (PtrDecrement n1 : PtrDecrement n2 : xs) = squash (PtrDecrement (n1 + n2) : xs)
-squash (a : xs) = a : squash xs
+squash (x : xs) = x : squash xs
 squash [] = []
