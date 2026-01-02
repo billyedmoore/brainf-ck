f70a1401fbad6034dafd76bc3d8353df0313aeb3
Billy Moore <billyedmoore@yahoo.com>
2026-01-02 18:20:47 +0000

    Remove arithmetic (Ptr and Data) that has no effect.

--- src/BrainFuck/Parse.hs
+++ src/BrainFuck/Parse.hs
@@ -44,8 +44,8 @@ basicParse [] = Right []
 basicParse (c : _) = Left $ UnexpectedSymbol c
 
 squash :: [BrainFuckAST] -> [BrainFuckAST]
-squash (DataArithmetic n1 : DataArithmetic n2 : xs) = squash (DataArithmetic (n1 + n2) : xs)
-squash (PtrArithmetic n1 : PtrArithmetic n2 : xs) = squash (PtrArithmetic (n1 + n2) : xs)
+squash (DataArithmetic n1 : DataArithmetic n2 : xs) = if n1 + n2 == 0 then squash xs else squash (DataArithmetic (n1 + n2) : xs)
+squash (PtrArithmetic n1 : PtrArithmetic n2 : xs) = if n1 + n2 == 0 then squash xs else squash (PtrArithmetic (n1 + n2) : xs)
 squash (Loop body : xs) = Loop (squash body) : squash xs
 squash (x : xs) = x : squash xs
 squash [] = []
