c854192823a99dc9ed36b292689ca1ead010f5fd
Billy Moore <billyedmoore@yahoo.com>
2026-01-02 13:20:26 +0000

    Option parsing to allow for interpretting and transpiling.

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # Brainf-ck
 
-Program to transpile code from the esoteric language [BrainFuck](https://en.wikipedia.org/wiki/Brainfuck)
-to (for now exclusively) Bash performing minor optimisation along the way (grouping
+Program to interpret and transpile code from the esoteric
+language [BrainFuck](https://en.wikipedia.org/wiki/Brainfuck)
+performing minor optimisation along the way (grouping
 sequential increments and decrements).
 
 ## General Usage
 
+To interpret programs simply omit the output file.
+
 ```bash
-# Generated Bash code is written to stdout
-brainf-ck your_program.bf > out.sh
+brainf-ck your_program.bf 
+```
+
+To transpile programs, the transpilation language will be infered
+from the file extension of the output file. Although since only bash 
+is supported that extension better be (`.sh` or `.bash`).
+
+```bash
+brainf-ck your_program.bf -o your_program.sh
 ```
 
 ## Run Tests
--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,63 @@
 module Main (main) where
 
--- import BrainFuck.Bash qualified as Bash
+import BrainFuck.Bash qualified as Bash
 import BrainFuck.Interpret qualified as Interpret
 import BrainFuck.Parse (parse)
-import System.Environment (getArgs)
+import Options.Applicative
 import System.Exit (die)
 
+takeExtension :: String -> String
+takeExtension s = takeExtensionInternal (reverse s) ""
+  where
+    takeExtensionInternal :: String -> String -> String
+    takeExtensionInternal ('.' : _) acc = acc
+    takeExtensionInternal (x : xs) acc = takeExtensionInternal xs (x : acc)
+    takeExtensionInternal [] _ = ""
+
+data Options = Options
+  { _inputFile :: String,
+    _outputFile :: Maybe String
+  }
+
+optionParser :: Parser Options
+optionParser =
+  Options
+    <$> strArgument
+      ( metavar "INPUT_FILE"
+          <> help "Input BrainFuck file."
+      )
+    <*> optional
+      ( strOption
+          ( long "output"
+              <> metavar "OUTPUT_FILE"
+              <> short 'o'
+              <> help
+                ( "Output file, language of transpilation will be decided by file extension."
+                    <> " If omitted the input file will be interpretted directly."
+                )
+          )
+      )
+
 main :: IO ()
-main = do
-  args <- getArgs
+main = programMain =<< execParser opts
+  where
+    opts =
+      info
+        (optionParser <**> helper)
+        ( fullDesc
+            <> progDesc "Transpile or interpret BrainFuck programs."
+            <> header "brainf-ck - a BrainFuck interpreter and transpiler."
+        )
 
-  case args of
-    [filePath] -> do
-      content <- readFile filePath
-
-      case parse content of
-        Left err ->
-          die $ "Parse Error: " ++ show err
-        Right ast -> do
-          Interpret.interpret ast
-    -- let result = Bash.compile ast
-    -- putStrLn result
-    _ -> putStrLn "Usage: brainf-ck <bf-file>"
+programMain :: Options -> IO ()
+programMain (Options inputFile maybeOutputFile) = do
+  content <- readFile inputFile
+  case parse content of
+    Left err ->
+      die $ "Parse Error: " ++ show err
+    Right ast ->
+      case maybeOutputFile of
+        (Just outputFile) -> case takeExtension outputFile of
+          s | s `elem` ["sh", "bash"] -> writeFile outputFile (Bash.compile ast)
+          _ -> die $ "Unsupported File Type: " ++ show (takeExtension outputFile)
+        Nothing -> Interpret.interpret ast
